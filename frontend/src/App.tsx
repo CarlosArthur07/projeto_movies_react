3ae7b4251244293a2cabca4ecd075b062ec030e6
CarlosArthur07 <carlosarthurcs05@gmail.com>
2024-11-06 18:42:39 -0300

    Projeto criado em sala de aula

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Listing from '../src/pages/Listing';
+import Form from '../src/pages/Form';
+
+
+function App() {
+  return ( 
+    <BrowserRouter>
+     <Navbar/>
+     <Routes>
+      <Route path='/' element={<Listing/>}/>
+      <Route path='/form'>
+      <Route path=":moviesId" element={<Form/>}/>
+      </Route>
+      </Routes>
+    </BrowserRouter>
+);
+}
+
+export default App;
