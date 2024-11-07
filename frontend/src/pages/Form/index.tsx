a5aa01977e1ca5bb6ec93d5f6785a1032067bd02
CarlosArthur07 <carlosarthurcs05@gmail.com>
2024-11-06 21:40:28 -0300

    Pagination e componentes criados

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,54 +1,52 @@
 import { count } from "console";
 import { title } from "process";
+import "./styles.css";
 
-function Form(){
-    const movies = {
-        id: 1,
-        image:"https://media.themoviedb.org/t/p/w300_and_h450_bestv2/cuFhVLPJ9zC06EMV5XAKNNRJtC4.jpg",
-        title:"Transformers: O Início (2024)",
-        count:2,
-        score: 4.0
+function Form() {
+  const movies = {
+    id: 1,
+    image:
+      "https://media.themoviedb.org/t/p/w300_and_h450_bestv2/cuFhVLPJ9zC06EMV5XAKNNRJtC4.jpg",
+    title: "Transformers: O Início (2024)",
+    count: 2,
+    score: 4.0,
+  };
 
-    };
+  return (
+    <div className="movies-form-container">
+      <img
+        className="movies-card-image"
+        src={movies.image}
+        alt="Transformers: O Início (2024)"
+      />
+      <div className="movies-card-bottom-container">
+        <h3>Transformers: O Início (2024)</h3>
+        <form className="movies-form">
+          <div className="form-group movies-form-group">
+            <label htmlFor="email">informe seu e-mail</label>
+            <input className="form-control" type="email" id="email" />
+          </div>
 
-    return(
-        <div className="movies-form-container">
-        <img src={movies.image} alt="Transformers: O Início (2024)"/>
-        <div className="movies-card-bottom-container">
-            <h3>
-            Transformers: O Início (2024)
-            </h3>
-            <form className="movies-form">
-              <div className="form-group movies-form-group">
-                <label htmlFor="email">
-                  informe seu e-mail
-                </label>
-                <input className="form-control" type="email" id="email"/>
-                </div>
-
-                <div className="form-group movies-form-group">
-                 <label htmlFor="score">
-                  informe sua avaliação
-                 </label>
-                 <select className="form-control" id="score">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-
-                 </select>
-                 </div>
-	     <div className="movies-form-btn-container"/>
-              <button className="btn movies-btn" type="submit">SALVAR</button>
-             </form>
-              <button className=" btn movies-btn mt-3">
-                CANCELAR
-              </button>
-            </div>                
-           </div>
-
-    );
+          <div className="form-group movies-form-group">
+            <label htmlFor="score">informe sua avaliação</label>
+            <select className="form-control" id="score">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
+          </div>
+          <div className="movies-form-btn-container">
+            <button className="btn movies-btn" type="submit">
+              SALVAR
+            </button>
+          </div>
+        </form>
+        <button className=" btn movies-btn mt-3">CANCELAR</button>
+      </div>
+    </div>
+  );
 }
 
 export default Form;
